4b32ebf6f577f10756f6d5a63477bc7a5d117286
Thomas Leonard <thomas.leonard@docker.com>
2017-02-13 15:58:11 +0000

    ci: record history of builds
    Each time a job's output changes to a new state, it is recorded in a
    branch named after the target. There is one directory per job, with an
    output file holding JSON of the status and the log tree.
    
    Signed-off-by: Thomas Leonard <thomas.leonard@docker.com>

--- ci/src/cI_output.mli
+++ ci/src/cI_output.mli
@@ -18,3 +18,11 @@ val result : 'a t -> 'a CI_result.t
 val logs : 'a t -> logs
 val status : _ t -> [`Success | `Pending | `Failure]
 val descr : string t -> string
+
+val equal : string t -> string t -> bool
+(** [equal a b] is [true] iff [a] and [b] are equal for the purposes of saving the output metadata to disk.
+    i.e. they have the same result and the same log trees: live logs must be the same object and saved logs
+    must be the same commit. *)
+
+val json_of : string t -> Yojson.Basic.json
+val of_json : Yojson.Basic.json -> string t
